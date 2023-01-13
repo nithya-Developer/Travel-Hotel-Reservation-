21bff48dc1323b6bc75a7c3500aab25d928b693c
nithya-Developer <nithi.nithyab@gmail.com>
2023-01-13 19:51:37 +0530

    adding header

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,22 +1,24 @@
-import {   faBed,
+import {
+  faBed,
   faCalendarDays,
   faCar,
   faPerson,
   faPlane,
-  faTaxi } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "./header.css"
-
+  faTaxi,
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './header.css'
 
 const Header = () => {
   return (
     <div className="header">
-      <div className="headerList">
-        <div className="headerListItem">
-          <FontAwesomeIcon icon={faBed} />
-          <span>Stays</span>
-        </div>
-        <div className="headerListItem">
+      <div className="headerContainer">
+        <div className="headerList">
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+          </div>
+          <div className="headerListItem">
             <FontAwesomeIcon icon={faPlane} />
             <span>Flights</span>
           </div>
@@ -32,6 +34,7 @@ const Header = () => {
             <FontAwesomeIcon icon={faTaxi} />
             <span>Airport taxis</span>
           </div>
+        </div>
       </div>
     </div>
   )
