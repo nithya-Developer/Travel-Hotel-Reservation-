39efb45a778b415aa4a5f4aec86d01f495865bbe
nithya-Developer <nithi.nithyab@gmail.com>
2023-01-13 18:59:59 +0530

    adding

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,4 +1,5 @@
-import Navbar from '../../components/navbar/Navbar'
+import Navbar from '../../components/navbar/Navbar';
+import Header from '../../components/header/Header';
 import "./home.css"
 
 
@@ -6,6 +7,7 @@ const Home = () => {
   return (
     <div>
       <Navbar />
+      <Header />
     </div>
   )
 }
