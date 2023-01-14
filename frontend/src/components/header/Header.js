ee17c1a49085e0e76b80341a1aaf873b407ceaf7
nithya-Developer <nithi.nithyab@gmail.com>
2023-01-14 18:17:28 +0530

    header sec

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
     <div className="header">
       <div className="headerContainer">
         <div className="headerList">
-          <div className="headerListItem">
+          <div className="headerListItem active">
             <FontAwesomeIcon icon={faBed} />
             <span>Stays</span>
           </div>
@@ -33,6 +33,31 @@ const Header = () => {
           <div className="headerListItem">
             <FontAwesomeIcon icon={faTaxi} />
             <span>Airport taxis</span>
+          </div>
+        </div>
+        <h1 className="headerTitle">A lifetime of discounts? It's Genius.</h1>
+        <p className="headerDesc">
+          Get rewarded for your travels – unlock instant savings of 10% or more
+          with a free TripoTravel account
+        </p>
+        <button className="headerBtn">Sign in / Register</button>
+        <div className="headerSearch">
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faBed} className="headerIcon"/>
+            <input
+                  type="text"
+                  placeholder="Where are you going?"
+                  className="headerSearchInput" />
+
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faCalendarDays} className="headerIcon"/>
+            <span className="headerSearchItem">date to date</span>
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faPerson} className="headerIcon"/>
+            <span className="headerSearchItem">2 adults 2 children 1 room</span>
+
           </div>
         </div>
       </div>
