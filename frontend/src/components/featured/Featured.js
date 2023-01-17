ca7dd4e9eac19ad3284bcd707af67c7da82b84f6
nithya-Developer <nithi.nithyab@gmail.com>
2023-01-17 20:41:10 +0530

    done with featured sec

--- frontend/src/components/featured/Featured.js
+++ frontend/src/components/featured/Featured.js
@@ -2,7 +2,40 @@ import "./featured.css"
 
 const featured = () => {
   return (
-    <div>featured</div>
+    <div className="featured">
+        <div className="featuredItem">
+            <img 
+                src="https://i.ibb.co/dDjPpLj/image-1.webp"
+                alt="delhi"  
+                className="feacturedImg" />
+            <div className="featuredTitles">
+                <h1> Bangalore</h1>
+                <h2> 533 properties</h2>
+            </div>
+        </div>
+        <div className="featuredItem">
+            <img 
+                src="https://i.ibb.co/PCJ766r/dubai.png" 
+                alt="dubai"
+                className="feacturedImg" />
+            <div className="featuredTitles">
+                <h1>Dubai </h1>
+                <h2> 533 properties</h2>
+            </div>
+        </div>
+        <div className="featuredItem">
+            <img 
+                src="https://i.ibb.co/f0rJbCB/paris.png" 
+                alt="paris" 
+                className="feacturedImg" 
+            />
+            <div className="featuredTitles">
+                <h1> Paris</h1>
+                <h2> 533 properties</h2>
+            </div>
+        </div>
+
+    </div>
   )
 }
 
