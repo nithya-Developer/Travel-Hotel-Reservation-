ecb0ad64007d64cd425344df6a7365ed07dc36f1
nithya-Developer <nithi.nithyab@gmail.com>
2023-01-18 13:30:10 +0530

    fPro sec done

--- frontend/src/components/featuredProperties/FeaturedProperties.js
+++ frontend/src/components/featuredProperties/FeaturedProperties.js
@@ -1,8 +1,65 @@
-import "./featuredProperties.css"
+import './featuredProperties.css'
 
 const FeaturedProperties = () => {
   return (
-    <div>FeaturedProperties</div>
+    <div className="fp">
+      <div className="fpItem">
+        <img
+          src="https://i.ibb.co/yBhTckf/image-s3.png"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Aparthotel Stare Miasto</span>
+        <span className="fpCity">Madrid</span>
+        <span className="fpPrice">Starting from ₹&nbsp;2,846</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://i.ibb.co/J5JMQb3/image-s6.png"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Comfort Suites Airport</span>
+        <span className="fpCity">Austin</span>
+        <span className="fpPrice">Starting from ₹&nbsp;12,845</span>
+        <div className="fpRating">
+          <button>9.3</button>
+          <span>Exceptional</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://i.ibb.co/sWPz9WF/image-s2.png"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Four Seasons Hotel</span>
+        <span className="fpCity">Lisbon</span>
+        <span className="fpPrice">Starting from ₹&nbsp;3,399</span>
+        <div className="fpRating">
+          <button>8.8</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://i.ibb.co/T1zk70G/banner-3.jpg"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Hilton Garden Inn</span>
+        <span className="fpCity">Berlin</span>
+        <span className="fpPrice">Starting from ₹&nbsp;2,500</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
