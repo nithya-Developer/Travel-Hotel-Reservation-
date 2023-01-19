d9efe5fdd441ec384901139f9c42c8ef9f34964c
nithya-Developer <nithi.nithyab@gmail.com>
2023-01-19 14:45:41 +0530

    updated list css

--- frontend/src/pages/list/List.js
+++ frontend/src/pages/list/List.js
@@ -1,12 +1,16 @@
-import './list.css'
-import Navbar from '../../components/navbar/Navbar'
-import Header from '../../components/header/Header'
-import { useLocation } from "react-router-dom"
-
+import './list.css';
+import Navbar from '../../components/navbar/Navbar';
+import Header from '../../components/header/Header';
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import { format } from "date-fns"
 
 const List = () => {
 
-  const location = useLocation()
+  const location = useLocation();
+  const [destination, setDestination] = useState(location.state.destination);
+  const [date, setDate] = useState(location.state.date);
+  const [options, setOptions] = useState(location.state.options);
 
   console.log(location)
 
@@ -24,6 +28,10 @@ const List = () => {
             </div>
             <div className="lsItem">
               <label>Check-in Date</label>
+              <span>{`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(
+                  date[0].endDate, 
+                  "MM/dd/yyyy"
+                )}`}</span>
             </div>
           </div>
           <div className="listResult"></div>
