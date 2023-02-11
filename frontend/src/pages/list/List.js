e53ee794683c65655598c9517dd8b66266e596ca
nithya-Developer <nithi.nithyab@gmail.com>
2023-02-11 15:14:49 +0530

    apis working on listing page

--- frontend/src/pages/list/List.js
+++ frontend/src/pages/list/List.js
@@ -6,6 +6,7 @@ import { useState } from 'react';
 import { format } from 'date-fns';
 import { DateRange } from 'react-date-range';
 import SearchItem from '../../components/searchItem/SearchItem'
+import useFetch from "../../hooks/useFetch";
 
 const List = () => {
   const location = useLocation()
@@ -13,9 +14,17 @@ const List = () => {
   const [date, setDate] = useState(location.state.date)
   const [openDate, setOpenDate] = useState(false)
   const [options, setOptions] = useState(location.state.options)
+  const [min, setMin] = useState(undefined)
+  const [max, setMax] = useState(undefined);
+
+  const { data, loading, error, reFetch } = useFetch(
+    `/hotels?city=${destination}&min=${min ||0 }&max=${max || 999}`)
 
   // console.log(location)
 
+  const handleClick = ()=>{
+    reFetch()
+  }
   return (
     <div>
       <Navbar />
@@ -32,8 +41,8 @@ const List = () => {
               <label>Check-in Date</label>
               <span onClick={() => setOpenDate(!openDate)}>{`${format(
                 date[0].startDate,
-                'MM/dd/yyyy',
-              )} to ${format(date[0].endDate, 'MM/dd/yyyy')}`}</span>
+                "MM/dd/yyyy"
+              )} to ${format(date[0].endDate, "MM/dd/yyyy")}`}</span>
               {openDate && (
                 <DateRange
                   onChange={(item) => setDate([item.selection])}
@@ -45,63 +54,72 @@ const List = () => {
             <div className="lsItem">
               <label>Options</label>
               <div className="lsOptions">
-              <div className="lsOptionItem">
-                <sapn className="lsOptionText">
-                  Min price <small>per night</small>
-                </sapn>
-                <input type="number" className="lsOptionInput" />
+                <div className="lsOptionItem">
+                  <sapn className="lsOptionText">
+                    Min price <small>per night</small>
+                  </sapn>
+                  <input
+                    type="number"
+                    onChange={(e) => setMin(e.target.value)}
+                    className="lsOptionInput"
+                  />
+                </div>
+                <div className="lsOptionItem">
+                  <sapn className="lsOptionText">
+                    Max price <small>per night</small>
+                  </sapn>
+                  <input
+                    type="number"
+                    onChange={(e) => setMax(e.target.value)}
+                     className="lsOptionInput"
+                  />
+                </div>
+                <div className="lsOptionItem">
+                  <sapn className="lsOptionText">Adult</sapn>
+                  <input
+                    type="number"
+                    min={1}
+                    className="lsOptionInput"
+                    placeholder={options.adult}
+                  />
+                </div>
+                <div className="lsOptionItem">
+                  <sapn className="lsOptionText">Children</sapn>
+                  <input
+                    type="number"
+                    min={0}
+                    className="lsOptionInput"
+                    placeholder={options.children}
+                  />
+                </div>
+                <div className="lsOptionItem">
+                  <sapn className="lsOptionText">Room</sapn>
+                  <input
+                    type="number"
+                    min={1}
+                    className="lsOptionInput"
+                    placeholder={options.room}
+                  />
+                </div>
               </div>
-              <div className="lsOptionItem">
-                <sapn className="lsOptionText">
-                  Max price <small>per night</small>
-                </sapn>
-                <input type="number" className="lsOptionInput" />
-              </div>
-              <div className="lsOptionItem">
-                <sapn className="lsOptionText">Adult</sapn>
-                <input
-                  type="number"
-                  min={1}
-                  className="lsOptionInput"
-                  placeholder={options.adult}
-                />
-              </div>
-              <div className="lsOptionItem">
-                <sapn className="lsOptionText">Children</sapn>
-                <input
-                  type="number"
-                  min={0}
-                  className="lsOptionInput"
-                  placeholder={options.children}
-                />
-              </div>
-              <div className="lsOptionItem">
-                <sapn className="lsOptionText">Room</sapn>
-                <input
-                  type="number"
-                  min={1}
-                  className="lsOptionInput"
-                  placeholder={options.room}
-                />
-              </div>
-             </div> 
             </div>
-            <button>Search</button>
+            <button onClick={handleClick}>Search</button>
           </div>
           <div className="listResult">
-            <SearchItem/>
-            <SearchItem/>
-            <SearchItem/>
-            <SearchItem/>
-            <SearchItem/>
-            <SearchItem/>
-            <SearchItem/>
-            <SearchItem/>
+            {loading ? (
+              "loading"
+            ) : (
+              <>
+                {data.map((item) => (
+                  <SearchItem item={item} key={item._id} />
+                ))}
+              </>
+            )}
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default List
