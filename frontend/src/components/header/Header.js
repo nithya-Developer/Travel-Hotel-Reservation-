4e8109c13fcc833f14a8101047f7d0c8b9ff9ace
nithya-Developer <nithi.nithyab@gmail.com>
2023-02-13 14:44:55 +0530

    done with data range room

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -5,53 +5,57 @@ import {
   faPerson,
   faPlane,
   faTaxi,
-} from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import './header.css'
-import { DateRange } from 'react-date-range'
-import { useState } from 'react'
-import 'react-date-range/dist/styles.css' // main css file
-import 'react-date-range/dist/theme/default.css' // theme css file
-import { format } from 'date-fns'
-import { useNavigate } from 'react-router-dom'
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./header.css";
+import { DateRange } from "react-date-range";
+import { useContext, useState } from "react";
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { format } from "date-fns";
+import { useNavigate } from "react-router-dom";
+import { SearchContext } from "../../context/SearchContext";
 
 const Header = ({ type }) => {
-  const [destination, setDestination] = useState('')
-  const [openDate, setOpenDate] = useState(false)
-  const [date, setDate] = useState([
+  const [destination, setDestination] = useState("");
+  const [openDate, setOpenDate] = useState(false);
+  const [dates, setDates] = useState([
     {
       startDate: new Date(),
       endDate: new Date(),
-      key: 'selection',
+      key: "selection",
     },
-  ])
-  const [openOptions, setOpenOptions] = useState(false)
+  ]);
+  const [openOptions, setOpenOptions] = useState(false);
   const [options, setOptions] = useState({
     adult: 1,
     children: 0,
     room: 1,
-  })
+  });
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleOption = (name, operation) => {
     setOptions((prev) => {
       return {
         ...prev,
-        [name]: operation === 'i' ? options[name] + 1 : options[name] - 1,
-      }
-    })
-  }
+        [name]: operation === "i" ? options[name] + 1 : options[name] - 1,
+      };
+    });
+  };
+
+  const { dispatch } = useContext(SearchContext);
 
   const handleSearch = () => {
-    navigate('/hotels', { state: { destination, date, options } })
-  }
+    dispatch({ type: "NEW_SEARCH", payload: { destination, dates, options } });
+    navigate("/hotels", { state: { destination, dates, options } });
+  };
 
   return (
     <div className="header">
       <div
         className={
-          type === 'list' ? 'headerContainer listMode' : 'headerContainer'
+          type === "list" ? "headerContainer listMode" : "headerContainer"
         }
       >
         <div className="headerList">
@@ -76,7 +80,7 @@ const Header = ({ type }) => {
             <span>Airport taxis</span>
           </div>
         </div>
-        {type !== 'list' && (
+        {type !== "list" && (
           <>
             <h1 className="headerTitle">
               A lifetime of discounts? It's Genius.
@@ -101,16 +105,16 @@ const Header = ({ type }) => {
                 <span
                   onClick={() => setOpenDate(!openDate)}
                   className="headerSearchText"
-                >{`${format(date[0].startDate, 'MM/dd/yyyy')} to ${format(
-                  date[0].endDate,
-                  'MM/dd/yyyy',
+                >{`${format(dates[0].startDate, "MM/dd/yyyy")} to ${format(
+                  dates[0].endDate,
+                  "MM/dd/yyyy"
                 )}`}</span>
                 {openDate && (
                   <DateRange
                     editableDateInputs={true}
-                    onChange={(item) => setDate([item.selection])}
+                    onChange={(item) => setDates([item.selection])}
                     moveRangeOnFirstSelection={false}
-                    ranges={date}
+                    ranges={dates}
                     className="date"
                     minDate={new Date()}
                   />
@@ -130,7 +134,7 @@ const Header = ({ type }) => {
                         <button
                           disabled={options.adult <= 1}
                           className="optionCounterButton"
-                          onClick={() => handleOption('adult', 'd')}
+                          onClick={() => handleOption("adult", "d")}
                         >
                           -
                         </button>
@@ -139,7 +143,7 @@ const Header = ({ type }) => {
                         </span>
                         <button
                           className="optionCounterButton"
-                          onClick={() => handleOption('adult', 'i')}
+                          onClick={() => handleOption("adult", "i")}
                         >
                           +
                         </button>
@@ -151,7 +155,7 @@ const Header = ({ type }) => {
                         <button
                           disabled={options.children <= 0}
                           className="optionCounterButton"
-                          onClick={() => handleOption('children', 'd')}
+                          onClick={() => handleOption("children", "d")}
                         >
                           -
                         </button>
@@ -160,7 +164,7 @@ const Header = ({ type }) => {
                         </span>
                         <button
                           className="optionCounterButton"
-                          onClick={() => handleOption('children', 'i')}
+                          onClick={() => handleOption("children", "i")}
                         >
                           +
                         </button>
@@ -172,7 +176,7 @@ const Header = ({ type }) => {
                         <button
                           disabled={options.room <= 1}
                           className="optionCounterButton"
-                          onClick={() => handleOption('room', 'd')}
+                          onClick={() => handleOption("room", "d")}
                         >
                           -
                         </button>
@@ -181,7 +185,7 @@ const Header = ({ type }) => {
                         </span>
                         <button
                           className="optionCounterButton"
-                          onClick={() => handleOption('room', 'i')}
+                          onClick={() => handleOption("room", "i")}
                         >
                           +
                         </button>
@@ -200,7 +204,7 @@ const Header = ({ type }) => {
         )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- frontend/src/pages/hotel/Hotel.js
+++ frontend/src/pages/hotel/Hotel.js
@@ -1,19 +1,19 @@
-import './hotel.css'
-import Navbar from '../../components/navbar/Navbar'
-import Header from '../../components/header/Header'
-import MailList from '../../components/mailList/MailList'
-import Footer from '../../components/footer/Footer'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import "./hotel.css";
+import Navbar from "../../components/navbar/Navbar";
+import Header from "../../components/header/Header";
+import MailList from "../../components/mailList/MailList";
+import Footer from "../../components/footer/Footer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCircleArrowLeft,
   faCircleArrowRight,
   faCircleXmark,
   faLocationDot,
-} from '@fortawesome/free-solid-svg-icons'
-import { useState } from 'react'
+} from "@fortawesome/free-solid-svg-icons";
+import { useContext, useState } from "react";
 import useFetch from "../../hooks/useFetch";
 import { useLocation } from "react-router-dom";
-
+import { SearchContext } from "../../context/SearchContext";
 
 const Hotel = () => {
   const location = useLocation();
@@ -23,9 +23,16 @@ const Hotel = () => {
 
   const { data, loading, error } = useFetch(`/hotels/find/${id}`);
 
-  // const { dates } = useContext(SearchContext);
-  // console.log(dates)
+  const { dates, options } = useContext(SearchContext);
+  // console.log(dates);
+  const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24;
+  function dayDifference(date1, date2) {
+    const timeDiff = Math.abs(date2.getTime() - date1.getTime());
+    const diffDays = Math.ceil(timeDiff / MILLISECONDS_PER_DAY);
+    return diffDays;
+  }
 
+  const days = dayDifference(dates[0].endDate, dates[0].startDate);
 
   // const photos = [
   //   {
@@ -55,21 +62,21 @@ const Hotel = () => {
   // ]
 
   const handleOpen = (i) => {
-    setSlideNumber(i)
-    setOpen(true)
-  }
+    setSlideNumber(i);
+    setOpen(true);
+  };
 
   const handleMove = (direction) => {
-    let newSlideNumber
+    let newSlideNumber;
 
-    if (direction === 'l') {
-      newSlideNumber = slideNumber === 0 ? 5 : slideNumber - 1
+    if (direction === "l") {
+      newSlideNumber = slideNumber === 0 ? 5 : slideNumber - 1;
     } else {
-      newSlideNumber = slideNumber === 5 ? 0 : slideNumber + 1
+      newSlideNumber = slideNumber === 5 ? 0 : slideNumber + 1;
     }
 
-    setSlideNumber(newSlideNumber)
-  }
+    setSlideNumber(newSlideNumber);
+  };
 
   return (
     <div>
@@ -137,13 +144,13 @@ const Hotel = () => {
                 <p className="hotelDesc">{data.desc}</p>
               </div>
               <div className="hotelDetailsPrice">
-                <h1>Perfect for a 9-night stay!</h1>
+                <h1>Perfect for a {days}-night stay!</h1>
                 <span>
                   Located in the real heart of Krakow, this property has an
                   excellent location score of 9.8!
                 </span>
                 <h2>
-                  <b>$945</b> (9 nights)
+                  <b>₹&nbsp;{days, data.cheapestPrice * options.room}</b> ({days} nights)
                 </h2>
                 <button>Reserve or Book Now!</button>
               </div>
@@ -155,6 +162,6 @@ const Hotel = () => {
       )}
     </div>
   );
-}
+};
 
-export default Hotel
+export default Hotel;
